c97356cc666c5fbd2a0b8833d36eedaca801091e
JvDiesen-Curio <109534514+JvDiesen-Curio@users.noreply.github.com>
2023-12-20 13:23:29 +0100

    week 4

---
+++ src/controllers/class/controller.ts
@@ -0,0 +1,24 @@
+export class Controller {
+    constructor() { }
+
+
+    public async get(id?: string): Promise<object[] | object> {
+        return []
+    }
+
+    public async post(data: object | null): Promise<object> {
+        return {}
+    }
+
+    public async update(id: string, data: object | null): Promise<object> {
+        return {}
+    }
+
+    public async delete(id: string): Promise<boolean> {
+        return false
+    }
+
+
+}
+
+
---
+++ src/routers/users.router.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { UserController } from "../controllers/user.controller";
+
+export const userRouter = Router()
+
+
+userRouter.get('/', (req, res) => {
+    new UserController().get().then(data => res.send(data)).catch(e => res.status(400).send({ msg: e.message }))
+});
+
+userRouter.get("/:id", (req, res) => {
+    new UserController().get(req.params.id).then(data => res.send(data)).catch(e => res.status(400).send({ msg: e.message }))
+})
+
+userRouter.post('/', (req, res) => {
+    new UserController().post(req.body).then(data => res.send(data)).catch(e => res.status(500).send({ msg: e.message }))
+})
+
+
+
